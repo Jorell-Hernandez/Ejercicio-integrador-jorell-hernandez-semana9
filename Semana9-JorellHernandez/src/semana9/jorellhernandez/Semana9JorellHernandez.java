b1fcd64003309ccfd998a673dea261d16575b2d7
Jorell-Hernandez <jhernandez20979@ufide.ac.cr>
2025-11-05 21:01:25 -0600

    Add files via upload

---
+++ Semana9-JorellHernandez/src/semana9/jorellhernandez/Semana9JorellHernandez.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana9.jorellhernandez;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Semana9JorellHernandez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int ventasDia[];
+        String productoVendido[] = null;
+        String dias[] = new String[7];
+        
+        for (int i = 0; i < 10; i++) {
+            productoVendido[i] = JOptionPane.showInputDialog("Digite el número de ventas:");
+        }
+        
+        
+    }
+
+}
